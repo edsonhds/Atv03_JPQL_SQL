aa9112180cd289e430dc93b2c379e222c932d028
Edson Henrique <100642760+edsonhds@users.noreply.github.com>
2023-11-15 12:58:04 -0300

    Add files via upload

---
+++ Atividade_03_JPQL_SQL/src/main/java/com/example/demo/Atividade_03_JPQL_SQL.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade_03_JPQL_SQL {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Atividade_03_JPQL_SQL.class, args);
+    }
+
+}
